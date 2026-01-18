63009a4449bf565160ce1d436734dd1fdd1afc0a
abdur rouf <abdurroufkpkh@gmail.com>
2026-01-18 18:52:16 +0600

    s

--- src/app/nari-shakti/page.js
+++ src/app/nari-shakti/page.js
@@ -547,7 +547,7 @@ export default function NariShaktiPage() {
     setLoading(true)
 
     try {
-      const res = await fetch(`${API_BASE}/api/woman-empower-cards`, {
+      const res = await fetch(`${API_BASE}/api/woman-empower-card`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
